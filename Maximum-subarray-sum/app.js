e750b1b099f125e6940ef0aa74f4318e30c15ec1
Luke Lai <lukelai.dev@gmail.com>
2022-02-12 23:25:58 +0000

    update solution

--- Maximum-subarray-sum/app.js
+++ Maximum-subarray-sum/app.js
@@ -16,5 +16,16 @@ var maxSequence = function(array){
   }
     return sum;
 }
+ 
+//another way
+function maxSequence2(arr){
+    var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
 
 console.log(maxSequence([-2,1,-3,4,-1,2,1,-5,4]));
