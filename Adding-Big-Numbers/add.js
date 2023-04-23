0a4f691bc7d3ea5620cb35f610b63c6ce5c0a4d9
Luke Lai <lukelai.dev@gmail.com>
2023-04-23 22:21:46 +0100

    Format style

--- Adding-Big-Numbers/add.js
+++ Adding-Big-Numbers/add.js
@@ -1,29 +1,24 @@
 function add(a, b) {
-  const sum = Number(a) + Number(b);
-  if (!sum.toString().includes(".")) {
-    return sum.toString();
-  } else {
-    const maxLen = Math.max(a.length, b.length);
-    let carry = 0;
-    let result = [];
+  const maxLen = Math.max(a.length, b.length);
+  let carry = 0;
+  let result = [];
 
-    for (let i = 0; i < maxLen; i++) {
-      const digitA = parseInt(a[a.length - 1 - i]) || 0;
-      const digitB = parseInt(b[b.length - 1 - i]) || 0;
-      let sum = digitA + digitB + carry;
-      if (sum >= 10) {
-        carry = 1;
-        sum = sum - 10;
-      } else {
-        carry = 0;
-      }
-      result.unshift(sum.toString());
+  for (let i = 0; i < maxLen; i++) {
+    const digitA = parseInt(a[a.length - 1 - i]) || 0;
+    const digitB = parseInt(b[b.length - 1 - i]) || 0;
+    let sum = digitA + digitB + carry;
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    } else {
+      carry = 0;
     }
-    if (carry > 0) {
-      result.unshift("1");
-    }
-    return result.join("");
+    result.unshift(sum.toString());
   }
+  if (carry > 0) {
+    result.unshift("1");
+  }
+  return result.join("");
 }
 
 module.exports = add;
